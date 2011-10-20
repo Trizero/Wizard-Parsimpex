b718c5518fc22f1596b1efe0433d5edd0ce1da06
Giorgio Polvara <polvara@gmail.com>
2011-10-20 11:57:01 +0200

    Creato repository

---
+++ benvenuto.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Benvenuto</title>
+  </head>
+  <body>
+    <h1>Benvenuto</h1>
+    <p>Grazie a questo form passo passo potrai creare una richiesta.</p>
+    <form>
+      <input type="button" value="Inizia" />
+    </form>
+  </body>
+</html>
